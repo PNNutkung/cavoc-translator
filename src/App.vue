dbd08c2373a7091017e406b95e567d6218a154c3
PNNutkung <pipatpol.t@ku.th>
2017-06-05 22:49:17 +0900

    :tada: Initialized file check-in

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <mu-appbar title="Cavoc translator"></mu-appbar>
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <mu-table id="translating-table" :showCheckbox="showCheckbox">
+      <mu-thead>
+        <mu-th>
+          Japanese
+        </mu-th>
+        <mu-th>
+          English
+        </mu-th>
+        <mu-th>
+          Thai
+        </mu-th>
+      </mu-thead>
+      <mu-tbody>
+        <mu-tr>
+          <mu-td>
+            nihon go
+          </mu-td>
+          <mu-td>
+            ei go
+          </mu-td>
+          <mu-td>
+            tai go
+          </mu-td>
+        </mu-tr>
+      </mu-tbody>
+    </mu-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      showCheckbox: false
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
