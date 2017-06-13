03a6f563f86bdd187aa3890718cd228e85ffbfd1
PNNutkung <pipatpol.t@ku.th>
2017-06-13 15:19:43 +0900

    Swap submit and cancel button

--- src/components/TranslatorModal.vue
+++ src/components/TranslatorModal.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <mu-flat-button label="Translate" @click="open" primary />
-    <mu-dialog title="Translate" :name="selectedWord.ja" :open="dialog" @close="close">
-      <mu-table id="translating-modal" :showCheckbox="showCheckbox" :selectable="selectable">
+    <mu-dialog id="translate-dialog" title="Translate" :name="selectedWord.ja" :open="dialog" @close="close">
+      <mu-table :showCheckbox="showCheckbox" :selectable="selectable">
         <mu-thead>
           <mu-th>
             <h2>Japanese</h2>
@@ -28,11 +28,13 @@
           </mu-tr>
         </mu-tbody>
       </mu-table>
-      <mu-checkbox label="Verify?" v-model="selectedWord.verified" id="modal-verify" name="word-verify" :value="selectedWord.verified"/>
+      <div id="verify-div">
+        <mu-checkbox label="Verify?" v-model="selectedWord.verified" id="modal-verify" name="word-verify" :value="selectedWord.verified"/>
+      </div>
       <div id="modal-btns">
         <mu-flat-button label="G Translate" @click="googleTransalateAPIPost()" />
-        <mu-flat-button label="Cancel" @click="cancel()" secondary />
         <mu-flat-button label="Submit" @click="submit(selectedWord)" primary />
+        <mu-flat-button label="Cancel" @click="cancel()" secondary />
       </div>
     </mu-dialog>
   </div>
@@ -80,10 +82,17 @@ export default {
 <style scoped>
   #modal-btns {
     display: flex;
+    flex-direction: row;
     justify-content: space-around;
   }
 
-  #modal-verify {
+  #translate-dialog {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+
+  #verify-div {
     display: flex;
     justify-content: center;
   }
