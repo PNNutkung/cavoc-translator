91d7e5444bbb6ee80c0ef8ea3ba59bf226350901
PNNutkung <pipatpol.t@ku.th>
2017-06-13 10:03:55 +0900

    Add AddWordModal

---
+++ src/components/AddWordModal.vue
@@ -0,0 +1,23 @@
+<template>
+  <modal :name="modalName">
+    <form action="">
+      <div class="add-new-word-modal">
+
+      </div>
+    </form>
+  </modal>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      modalName: 'Add new word'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
